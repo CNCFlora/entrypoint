c8f5e09a7111fa08d633e9c66fd6504e5de971fb
Diogo Souza <diogo@diogok.net>
2014-12-10 11:55:15 -0200

    links and apps updates

--- app.js
+++ app.js
@@ -9,19 +9,24 @@ $(function(){
             user=u;
             $("#login").hide();
             $("#who span").text(u.name);
-            for(var c in u.roles) {
-                html = "<li>"+u.roles[i].context+"<ul>";
-                for(var i in u.roles[c].roles) {
-                    var html = "<li>"+u.roles[c].roles[i].role+"<ul>";
-                    for(var ii in u.roles[c].roles[i].entities) {
-                        html += "<li>"+u.roles[c].roles[i].entities[ii].name+"</li>";
+            if(u.roles) {
+              for(var c in u.roles) {
+                  html = "<li>"+u.roles[c].context+"<ul>";
+                  if(u.roles[c].roles) {
+                    for(var i in u.roles[c].roles) {
+                        html += "<li>"+u.roles[c].roles[i].role+"<ul>";
+                        if(u.roles[c].entities) {
+                          for(var ii in u.roles[c].roles[i].entities) {
+                              html += "<li>"+u.roles[c].roles[i].entities[ii].name+"</li>";
+                          }
+                        }
+                        html += "</ul></li>";
                     }
-                    html += "</ul></li>";
-                }
-                html += "</ul></li>";
-                $("#who>ul").append(html);
+                  }
+                  html += "</ul></li>";
+                  $("#who>ul").append(html);
+              }
             }
-            //check();
         },
         onlogout: function() {
             $("#login").show();
@@ -30,6 +35,7 @@ $(function(){
     });
 
     $("#login").click(function(){  Connect.login(); });
+    $("#logout").click(function(){  Connect.logout(); });
 
 });
 
